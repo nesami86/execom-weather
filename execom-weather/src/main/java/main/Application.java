4cd9e188e8b32433520240cefab35289bed42e9f
Nenad Milovanov <nesami86@yahoo.com>
2015-02-02 13:37:41 +0100

    v 0.1

---
+++ execom-weather/src/main/java/main/Application.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ execom-weather/src/main/java/main/authentication/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package main.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * Intercepts users calls and filters them accordingly
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        
+//    @Autowired
+//    private UserDetailsServiceImpl userDetailsService;
+        
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+    
+    /**
+     * Authenticates registered users (from database)
+     */
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+//        auth.authenticationProvider(daoAuthenticationProvider);
+//    }
+}
