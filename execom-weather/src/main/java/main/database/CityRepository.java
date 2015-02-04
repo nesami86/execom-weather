2171b0563ae01b20594a4784a63b1ac6431c6539
Gabor Vadocki <gvadocki@gmail.com>
2015-02-04 08:44:24 +0100

    Minor changes to CityRepository.

--- execom-weather/src/main/java/main/database/CityRepository.java
+++ execom-weather/src/main/java/main/database/CityRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.CrudRepository;
 @Component
 public interface CityRepository extends CrudRepository<City, Long>{
 
-	//City findByCityID(Integer id);  //mora se zvat isto kao i polje u entitetu
+	City findByCityID(Integer id);
 }
